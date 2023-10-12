a00fd0d3049e184d351a8a964e45d3c3e2f2a20f
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-10-12 21:20:33 +0000

    Começo do site

---
+++ script.js
@@ -0,0 +1,9 @@
+const pinto_audio = document.querySelector(".pinto")
+const zap_audio = document.querySelector(".zap") 
+const scooby = document.querySelector(".scooby")
+scooby.addEventListener("click",(evt)=>{
+    pinto_audio.play()
+})
+/* addEventListener("click",()=>{
+    zap_audio.play()
+}) */
