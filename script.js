fdf23bbbcee4139d32cf2e251e720d36c4bf9a34
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-10-13 15:39:51 +0000

    Quase completo

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const sons = {
   vasco: document.querySelector(".vasco"),
   manga: document.querySelector(".manga"),
   pinto: document.querySelector(".pinto"),
+  among: document.querySelector(".among"),
 };
+
 const videos = {
   scooby: document.querySelector(".scooby"),
   comedor: document.querySelector(".comedor_cu"),
@@ -16,13 +18,46 @@ const videos = {
   core: document.querySelector(".core"),
   dokidoki: document.querySelector(".dokidoki"),
 };
-const aparecer = document.querySelector(".aparecer")
-function adicionarAudio(portador, som) {
-  portador.addEventListener("click", () => som.play());
+
+const imgsEnt = [
+  "imgs/gato_among.jpg",
+  "imgs/gato_among.jpg",
+  "imgs/gato_among.jpg",
+  "imgs/gato_among.jpg",
+  "imgs/gato_among.jpg",
+  "imgs/gato_among.jpg",
+  "imgs/gato_among.jpg",
+];
+
+const aparecer = document.querySelector(".aparecer");
+const fundo = document.querySelector(".fundo");
+
+function comecando() {
+  aparecer.style.display = "none";
+  fundo.style.display = "block";
 }
-function styleBlock(portador) {
-  portador.style.display = "block";
+
+function adicionarAudio(element, audio) {
+  element.addEventListener("click", () => audio.play());
 }
+
+function adicionarBotaoSurpresa() {
+  const botao = document.createElement("button");
+  botao.textContent = ":)";
+  aparecer.appendChild(botao);
+  botao.classList.add("botao_surpresa");
+}
+
+function trocarFundo(sorteador) {
+  aparecer.style.backgroundImage = `url(${imgsEnt[sorteador]})`;
+}
+
+function esconderVideos() {
+  for (const video of Object.values(videos)) {
+    video.style.display = "none";
+  }
+}
+
 adicionarAudio(videos.scooby, sons.pinto);
 adicionarAudio(videos.comedor, sons.zap);
 adicionarAudio(aparecer, sons.gato);
@@ -30,52 +65,63 @@ adicionarAudio(videos.mario, sons.elon);
 adicionarAudio(videos.gatos, sons.vasco);
 adicionarAudio(videos.core, sons.manga);
 adicionarAudio(videos.dokidoki, sons.tira);
+
 addEventListener("keydown", (evt) => {
   switch (evt.key) {
     case " ":
+      fundo.style.display = "block";
       const input = document.createElement("input");
-      aparecer.appendChild(input);
+      fundo.appendChild(input);
       break;
     case "K":
-      sons.zap_audio.play();
+      sons.zap.play();
       break;
     case "a":
       sons.vasco.play();
       break;
     case "r":
+      sons.pinto.play();
       break;
     case "e":
+      sons.tira.play();
       break;
     case "n":
+      sons.elon.play();
       break;
     case "Enter":
+      sons.among.play();
       const sorteador = Math.floor(Math.random() * 6);
-      const arrayVideos = Object.values(videos)
-      arrayVideos.forEach((video) => {
-        video.style.display = "none";
-      });
-      switch (sorteador) {
-        case 0:
-          styleBlock(videos.scooby);
-          break;
-        case 1:
-          styleBlock(videos.comedor);
-          break;
-        case 2:
-          styleBlock(videos.mario);
-          break;
-        case 3:
-          styleBlock(videos.gatos);
-          break;
-        case 4:
-          styleBlock(videos.dokidoki);
-          break;
-        case 5:
-          styleBlock(videos.core);
-          break;
-        default:
-          break;
+      const botao_surpresa = Math.floor(Math.random() * 11);
+      if (botao_surpresa == 1) {
+        adicionarBotaoSurpresa();
       }
+      trocarFundo(sorteador);
+      setTimeout(() => {
+        esconderVideos();
+        switch (sorteador) {
+          case 0:
+            videos.scooby.style.display = "block";
+            break;
+          case 1:
+            videos.comedor.style.display = "block";
+            break;
+          case 2:
+            videos.mario.style.display = "block";
+            break;
+          case 3:
+            videos.gatos.style.display = "block";
+            break;
+          case 4:
+            videos.dokidoki.style.display = "block";
+            break;
+          case 5:
+            videos.core.style.display = "block";
+            break;
+          default:
+            break;
+        }
+      }, 3000);
+      break;
     default:
       break;
   }
